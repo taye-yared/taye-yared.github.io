44f7db644247346eebc397adb0db8b63bbc88fbb
Yared Taye <Yared Taye>
2016-12-29 22:55:23 -0600

    setup react env

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                Hello World !!!
+                <RaisedButton label="Default" />
+            </div>
+        )
+    }
+} 
