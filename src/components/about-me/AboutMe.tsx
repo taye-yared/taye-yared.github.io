142e04962497517f6bbcc6c00a47d800a53327ce
Yared Taye <Yared Taye>
2017-01-19 15:34:55 -0600

    Actually added dist

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -16,7 +16,7 @@ export default class AboutMe extends Component<undefined, State>{
     render(){
         return(
             <div>
-                <div classname= "uiuc-background">
+                <div className="uiuc-background">
                     <h1 className="title-header">About Me</h1>
                     <img src={myImage} className="my-avatar" width={140} height={140}/>
                     <Item title="UIUC Undergrad" bodyText="Current Junior" />
